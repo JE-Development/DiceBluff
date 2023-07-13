566b70fd7c16fa688e652ed0c075b8fdd651a2e0
Jason <jm.enns04@gmail.com>
2023-07-13 16:12:01 +0200

    first upload

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script>
+
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import WebSocket from "@/components/WebSocket.vue";
+
+export default {
+  //npm run dev | npm run build
+  name: "App",
+  components: {WebSocket, HelloWorld, TheWelcome},
+
+  created() {
+  },
+
+  mounted() {
+
+  },
+  data() {
+    return {
+    }
+  },
+
+  updated() {
+  },
+
+  methods: {
+
+
+  }
+}
+</script>
+
+<template>
+
+  <main>
+    <WebSocket/>
+  </main>
+</template>
+
+<style scoped>
+</style>
